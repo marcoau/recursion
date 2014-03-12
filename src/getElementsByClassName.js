d569d44e982639472432e1f3b7013c0329d3dffe
Marco Au <marco.au.321@gmail.com>
2014-03-12 17:57:36 +0800

    Implemented getElementsByClassName function.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,15 @@
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
   // your code here
+  var elementList = [];
+  var pushRightElements = function(element){
+  	_.each(element.childNodes, function(object){
+  	 	if(object.classList !== undefined && object.classList.contains(className)){
+  	 		elementList.push(object);
+  	 	}
+  	 	pushRightElements(object);
+  	});
+  };
+  pushRightElements(document.body);
+  return elementList;
 };
