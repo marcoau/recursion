a528b5440a2675f64bf9857c6fcdd4f9ec0f90fb
Marco Au <marco.au.321@gmail.com>
2014-04-02 17:02:53 +0800

    Added detection for escape characters.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,7 +3,19 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function (json) {
+	console.log(json);
   // your code goes here
+  var escapee = {
+      '"':  '"',
+      '\\': '\\',
+      '/':  '/',
+      b:    'b',
+      f:    '\f',
+      n:    '\n',
+      r:    '\r',
+      t:    '\t'
+  };
+
   if(json !== undefined){
   	if(typeof json !== 'string'){
   		throw "Error: not a parse-able object";
@@ -13,8 +25,21 @@ var parseJSON = function (json) {
 	  	if(json[json.length - 1] !== '\"'){
 	  	  throw "Error: incomplete string";
 	  	}
+	  	var pos = 1;
+	  	var str = '';
 
-	  	return json.slice(1, json.length - 1);
+	  	while(pos < json.length - 1){
+	  		if(json[pos] === '\\' && typeof escapee[json[pos+1]] === 'string'){
+	  			//At the escape character
+	  			str += escapee[json[pos+1]];
+	  			pos += 2;
+	  		}else{
+	  			str += json[pos];
+	  			pos++;
+	  		}
+	  	}
+
+	  	return str;
 
 	  }else if(json[0] === '['){
 	  	//json is an array.
@@ -59,6 +84,7 @@ var parseJSON = function (json) {
 	  }else{
 	  	//json is a number.
 	  	if(json !== 'null' && isNaN(Number(json))){
+	  		console.log(json);
 	  		throw "Error: I don't know what you are referring to";
 	  	}
 	  	return json === 'null' ? null : Number(json);
